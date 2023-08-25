98c7d71cf887ef18e5faf7503baaad1a5669a0eb
Nicolas <enhorde@gmail.com>
2023-08-24 22:03:07 -0700

    Add parameter to let user use name of the file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import sys
 import re
 
-def read_pgn_file(file_path):
+def read_pgn_file(file_name):
+    file_path = f"{file_name}.pgn"
     with open(file_path, 'r') as pgn_file:
         pgn_content = pgn_file.read()
         pgn_content_no_evals = re.sub(r'\{ \[%eval [^\]]+\] \}', '', pgn_content)
@@ -8,5 +10,8 @@ def read_pgn_file(file_path):
         print(pgn_content_cleaned)
 
 if __name__ == "__main__":
-    pgn_file_path = "game.pgn"  # Replace with the path to your .pgn file
-    read_pgn_file(pgn_file_path)
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <name_of_the_game>")
+    else:
+        game_name = sys.argv[1]
+        read_pgn_file(game_name)
