4fe617df3095e2e277cfead7ff158aa66bb8a5a5
Nicolas <enhorde@gmail.com>
2023-08-24 21:53:28 -0700

    Remove evals

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import re
+
 def read_pgn_file(file_path):
     with open(file_path, 'r') as pgn_file:
         pgn_content = pgn_file.read()
-        print(pgn_content)
+        cleaned_content = re.sub(r'\{ \[%eval [^\]]+\] \}', '', pgn_content)
+        print(cleaned_content)
 
 if __name__ == "__main__":
-    pgn_file_path = "your_file.pgn"  # Replace with the path to your .pgn file
+    pgn_file_path = "game.pgn"  # Replace with the path to your .pgn file
     read_pgn_file(pgn_file_path)
