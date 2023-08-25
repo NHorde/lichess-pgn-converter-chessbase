61d8cd5ecf2f5171512ba7293e72f553977404f2
Nicolas <enhorde@gmail.com>
2023-08-24 22:00:14 -0700

    Final cleaning

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import re
 def read_pgn_file(file_path):
     with open(file_path, 'r') as pgn_file:
         pgn_content = pgn_file.read()
-        cleaned_content = re.sub(r'\{ \[%eval [^\]]+\] \}', '', pgn_content)
-        print(cleaned_content)
+        pgn_content_no_evals = re.sub(r'\{ \[%eval [^\]]+\] \}', '', pgn_content)
+        pgn_content_cleaned = re.sub(r'\d+\.\.\.\s*', '', pgn_content_no_evals)
+        print(pgn_content_cleaned)
 
 if __name__ == "__main__":
     pgn_file_path = "game.pgn"  # Replace with the path to your .pgn file
